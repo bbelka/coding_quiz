facc7c381e0c9162279aaca79bf348ece496e870
bbtwots <brettbelka@gmail.com>
2020-03-18 08:25:41 -0700

    closer

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 //answers array(s)
 //link questions to correct answers
 var questionArr = [
-    { question: "question1...?", options: ["answer1.1", "answer1.2", "answer1.3"], answer: "answer1.1" },
-    { question: "question2...?", options: ["answer2.1", "answer2.2", "answer2.3"], answer: "answer2.2" },
+    { question: "What is the HTML tag under which one can write the JavaScript code?", options: ["<javascript>", "<scripted>", "<script>", "<js>"], answer: "<script>" },
+    { question: "Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?", options: ["alertbox(“GeeksforGeeks”);", "msg(“GeeksforGeeks”);", "msgbox(“GeeksforGeeks”);", "alert(“GeeksforGeeks”);"], answer: "alert(“GeeksforGeeks”);" },
+    { question: "What is the correct syntax for referring to an external script called “geek.js”?", options: ["<script src=”geek.js”>", "<script src=”geek.js”>", "<script ref=”geek.js”>", "<script name=”geek.js”>"], answer: "<script src=”geek.js”>" },
+    { question: "The external JavaScript file must contain <script> tag. True or False?", options: ["True", "False"], answer: "False" },
+    { question: "Which of the following is not a reserved word in JavaScript?", options: ["interface", "throws", "program", "short"], answer: "program" },
+    { question: "What is the syntax for creating a function in JavaScript named as Geekfunc?", options: ["function = Geekfunc()", "function Geekfunc()", "function := Geekfunc()", "function : Geekfunc()"], answer: "function Geekfunc()" },
+    { question: "How is the function called in JavaScript?", options: ["call Geekfunc();", "call function GeekFunc();", "Geekfunc();", "function Geekfunc();"], answer: "Geekfunc();" },
+    { question: "How to write an ‘if’ statement for executing some code. If “i” is NOT equal to 8?", options: ["if(i<>5)", "if i<>5", "if(i!=5)", "if i!=5"], answer: "if(i!=5)" },
+    { question: "What is the correct syntax for adding comments in JavaScript?", options: ["<!–This is a comment–&gt", "//This is a comment", "–This is a comment", "**This is a comment**"], answer: "//This is a comment" },
+    { question: "What is the JavaScript syntax for printing values in Console?", options: ["print(5)", "console.log(5);", "console.print(5);", "print.console(5);"], answer: "console.log(5);" },
 ];
 var answerArr = [""];
 var headingEl = document.querySelector('#heading');
@@ -24,36 +32,38 @@ var correctAnsCounter = 0;
 
 
 document.getElementById("button1").addEventListener("click", function () {
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
-    
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            display.textContent = minutes + ":" + seconds;
-    
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
-    }
-    
-    button1.onclick = function () {
-        var fiveMinutes = 60 * 5,
-            display = document.querySelector('#time');
-        startTimer(fiveMinutes, display);
-    };
+    // function startTimer(duration, display) {
+    //     var timer = duration, minutes, seconds;
+    //     setInterval(function () {
+    //         minutes = parseInt(timer / 60, 10);
+    //         seconds = parseInt(timer % 60, 10);
+
+    //         minutes = minutes < 10 ? "0" + minutes : minutes;
+    //         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    //         display.textContent = minutes + ":" + seconds;
+
+    //         if (--timer < 0) {
+    //             timer = duration;
+    //         }
+    //     }, 1000);
+    // }
+
+    // button1.onclick = function () {
+    //     var fiveMinutes = 60 * 5,
+    //         display = document.querySelector('#time');
+    //     startTimer(fiveMinutes, display);
+    // };
     questionScreen();
-    
-});
-   
-document.getElementById("button3").addEventListener("click", function () {
-    localStorage.clear();
 
 });
+
+document.getElementById("button3").addEventListener("click", function () {
+    localStorage.clear();
+    scoreTable.innerHTML = "";
+});
+
+
 document.getElementById("button4").addEventListener("click", function () {
     questionCounter = 0;
     correctAnsCounter = 0;
@@ -61,6 +71,7 @@ document.getElementById("button4").addEventListener("click", function () {
 });
 
 document.getElementById("answerList").addEventListener("click", function () {
+    rightWrongEl.style.visibility = "visible";
     if (event.target.textContent === correctAns) {
         rightWrongEl.id = "response";
         rightWrongEl.textContent = "Correct!!";
@@ -79,7 +90,6 @@ document.getElementById("button2").addEventListener("click", function () {
     localStorage.setItem(initials, correctAnsCounter);
     highScoreScreen();
 });
-// todoForm.addEventListener("submit", addTodo);
 
 function clear() {
     headingEl.textContent = "";
@@ -93,6 +103,7 @@ function clear() {
     button2.style.visibility = "hidden";
     button3.style.visibility = "hidden";
     button4.style.visibility = "hidden";
+    scoreTable.innerHTML = ""
 
 }
 
@@ -124,7 +135,6 @@ function questionScreen() {
 
     if (questionCounter < questionArr.length) {
         clear();
-        button1.style.visibility = "visible";
 
         headingEl.textContent = "Question #" + (questionCounter + 1);
         console.log(headingEl);
@@ -140,10 +150,14 @@ function questionScreen() {
 
         listEl.textContent = "";
 
+        rightWrongEl.style.visibility = "visible";
+
         for (var i = 0; i < possArr.length; i++) {
             var node = document.createElement("LI");
+            var node1 = document.createElement("BUTTON")
             var textnode = document.createTextNode(possArr[i]);
-            node.appendChild(textnode);
+            node1.appendChild(textnode);
+            node.appendChild(node1);
             document.getElementById("answerList").appendChild(node);
         };
     }
@@ -184,17 +198,16 @@ function scoreSubmitScreen() {
 //start quiz over
 function highScoreScreen() {
     clear();
-
     for (var i = 0; i < localStorage.length; i++) {
+
         var node = document.createElement("TR");
         var node1 = document.createElement("TD");
-        var textnode1 = document.createTextNode(localStorage.key(i));
-        node1.appendChild(textnode1);
-        node.appendChild(node1);
-
         var node2 = document.createElement("TD");
+        var textnode1 = document.createTextNode(localStorage.key(i));
         var textnode2 = document.createTextNode(localStorage.getItem(localStorage.key(i)));
+        node1.appendChild(textnode1);
         node2.appendChild(textnode2);
+        node.appendChild(node1);
         node.appendChild(node2);
         document.getElementById("scoreTable").appendChild(node);
     };
